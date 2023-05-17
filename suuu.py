ee7d6707aada441d01d5c3961d268e89cae32d86
SIWON <siwongim033@stu.jejunu.ac.kr>
2023-05-18 04:15:45 +0900

    visual

--- suuu.py
+++ suuu.py
@@ -1,151 +1,86 @@
-from selenium import webdriver 
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-import time
 import matplotlib.pyplot as plt
-import requests
-from bs4 import BeautifulSoup
-
-path = (r".\chromedriver.exe")
-driver = webdriver.Chrome(path)
+from collections import Counter
+import csv
+import time
+import re
 
 def remove_duplicates(lst):
     return list(set(lst))
 
-answer=[]
+book_info_list = []
 name = []
-names = []
-tags=[]
-years=[] 
-y=[]
-links = []
-
+tags = []
+rt = []
+years = []
 query_txt = "디지털" #input("검색 단어 입력 : ") # 검색창에 검색할 단어 입력
 
-url = "https://search.kyobobook.co.kr/search?keyword=" + query_txt + "&target=total&gbCode=TOT&ra=qntt&len=100"
-keyword = "S000"
-driver.get(url) # 드라이버가 제어할 사이트
+path = r"C:\Users\siwon\Desktop\크롤링\chromedriver.exe"
+driver = webdriver.Chrome(path)
+driver.get(r"https://search.kyobobook.co.kr/search?keyword=%EB%94%94%EC%A7%80%ED%84%B8&target=total&gbCode=TOT&ra=qntt&len=100") # 드라이버가 제어할 사이트
 time.sleep(1) #1초 대기 -> 사이트가 다 로딩 되기 전에 뒤에 명령어가 실행되면 X
 
+time.sleep(3) #3초 대기
+links = [] #순회할 리스트
 
+while len(book_info_list) < 100:  # 100개의 정보를 수집할 때까지 반복
+    link_list = driver.find_elements_by_class_name("prod_info")
+    for link_element in link_list:
+        href = link_element.get_attribute('href')
+        if 'S000' in href:  # 링크에 'S000'이 포함되어 있는지 확인
+            links.append(href)
 
-# time.sleep(3) #3초 대기
-# links=[] #순회할 리스트
-# link_list = driver.find_elements_by_class_name("prod_info")
-# for i in  link_list:
-#     href = i.get_attribute('href')
-#     links.append(href)
-# linkss=remove_duplicates(links)
-# n=len(linkss)
-# print("링크수집완료")
+    linkss = remove_duplicates(links)
+    n = len(linkss)
+    print("링크 수집 완료")
 
-# for link in linkss:
-#     tags=[] #초기화
-#     year=[] #초기화
-#     time.sleep(3)
-#     driver.get(link)
-#     book_name = driver.find_elements_by_class_name('prod_title')
-#     name = book_name[0].text if len(book_name) > 0 else "책 제목 없음"
-#     print(name)
+    for link in linkss:
+        tags = []
+        year = []
+        time.sleep(2)
+        driver.get(link)
 
-    # book_time = driver.find_elements_by_xpath('//*[@id="scrollSpyProdInfo"]/div[13]/div[2]/table/tbody/tr[2]/td')
-    # for i in book_time:
-    #     year = i.text.split()[1]
+        div_element = driver.find_element_by_css_selector(".prod_info_text.publish_date")
+        text = div_element.text
+        pattern = r"(\d+년)"
+        match = re.search(pattern, text)
+        if match:
+            year = match.group(1)
+        else:
+            year = "텍스트를 찾을 수 없음"
+            
+        if year not in ['2017년', '2018년', '2019년', '2020년', '2021년', '2022년']:
+            continue
 
-    # book_cate = driver.find_elements_by_css_selector('.intro_book ul li a:nth-child(3)')
+        book_name = driver.find_elements_by_class_name('prod_title')
+        name = book_name[0].text if len(book_name) > 0 else "책 제목 없음"
 
-    # for j in range(n):
-    #     answer = [year, book_cate]
-    #     years.append(year)
-    #     tags.append(book_cate)
-    #     print(answer)
-    
-# years = []  # 빈 리스트 생성
-# category = []
-# for link in links:
-#     time.sleep(1)
-#     book_name = driver.find_elements_by_css_selector("div.prod_title_box > div.auto_overflow_contents > div > h1 > span")
-#     for i in book_name:
-#         name = i.text  
-#         names.append(name)
-#         if len(names) == nn:
-#             break                       
-#     book_time = driver.find_elements_by_xpath('//span[@class="date"]')
-#     for i in book_time:
-#         year = i.text.split()[0]  
-#         years.append(year)
-#         if len(years) == nn:
-#             break
-#     book_cate = driver.find_elements_by_css_selector('.intro_book ul li a:nth-child(3)')
-#     for i in book_cate:
-#         cate = i.text
-#         category.append(cate)
-#         if len(category) == nn:
-#             break
+        book_cate = driver.find_elements_by_css_selector('.intro_book ul li a:nth-child(3)')
+        categories = [elem.text for elem in book_cate]
 
-#     book_tag = driver.find_elements_by_css_selector(".tag") #키워드 추출, 변수에 저장
-#     for i in book_tag: #반복문 / 키워드 수만큼 반복
-#         title = i.text # 추출한 키워드를 title에 저장
-#         tags.append(title) #title을 리스트에 저장
-#         result = list(set(tags)) #반복되는 문자열 삭제
-        
+        book_tag = driver.find_elements_by_class_name('tab_text')
+        for element in book_tag:
+            tags.append(element.text)
+        rt = tags[13:-8]
+        rt = [elem for elem in rt if elem != "교환/반품/품절"]
 
-#     if len(years) == nn:
-#         break
+        book_info = [name, year, categories, rt]
 
-# print("책 제목:",names)  
-# print("발행연도:",years)
-# print("카테고리:",category)
+        book_info_list.append(book_info)
+        print(len(book_info_list))
 
+        if len(book_info_list) >= 100:
+            break
+
+    else : 
+        driver.get(r"https://search.kyobobook.co.kr/search?keyword=%EB%94%94%EC%A7%80%ED%84%B8&target=total&gbCode=TOT&page=2&ra=qntt&len=100")
+
+print(book_info_list)
 
 driver.quit()
-# for link in links:
-#     book_name = driver.find_element_by_class_name('prod_title')
-#     print(book_name.text)
-        
-# book_time = driver.find_elements_by_xpath('//span[@class="date"]') #출간일자를 수집할 변수
 
-# for i in book_time:
-#     year = i.text.split()[0]  # Split the text and get the year part
-#     if year in ['2017년', '2018년', '2019년', '2020년', '2021년', '2022년']:
-#         years.append(year)
-
-# book_time = driver.find_elements_by_xpath('//span[@class="date"]') #출간일자를 수집할 변수
-
-# for i in book_time:
-#     year = i.text.split()[0]  # Split the text and get the year part
-#     if year in ['2017년', '2018년', '2019년', '2020년', '2021년', '2022년']:
-#         years.append(year)
-
-# driver.get(r"https://search.kyobobook.co.kr/search?keyword="+query_txt+"&target=total&gbCode=TOT&page=2&ra=qntt&len=100")
-# book_time = driver.find_elements_by_xpath('//span[@class="date"]')
-
-# for i in book_time:
-#     year = i.text.split()[0]  # Split the text and get the year part
-#     if year in ['2017년', '2018년', '2019년', '2020년', '2021년', '2022년']:
-#         years.append(year)
-#     if len(years)==100:
-#         break
-
-# print("2017년 : ",years.count("2017년")) #years리스트 안에 2017인 값의 개수
-# print("2018년 : ",years.count("2018년")) #years리스트 안에 2018인 값의 개수
-# print("2019년 : ",years.count("2019년")) #years리스트 안에 2019인 값의 개수
-# print("2020년 : ",years.count("2020년")) #years리스트 안에 2020인 값의 개수
-# print("2021년 : ",years.count("2021년")) #years리스트 안에 2021인 값의 개수
-# print("2022년 : ",years.count("2022년")) #years리스트 안에 2022인 값의 개수
-# print(len(years))
-
-# # 그래프로 표현할 두 데이터 리스트 정의
-# sun = [2017, 2018, 2019, 2020, 2021, 2022]
-# scores = [years.count("2017년"), years.count("2018년"), 
-#           years.count("2019년"), years.count("2020년"), years.count("2021년"), years.count("2022년")]
-
-# plt.plot(sun, scores) # 사용할 데이터 리스트 선언
-
-# # X값과 y값으로 사용할 값 설정
-# plt.xlabel('Year')
-# plt.ylabel('Score')
-
-# plt.grid(True) # 눈금 표시
-
-# plt.show()# 그래프 보여주기
+with open('output.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(book_info_list)
