64b945bb7749b204a372b69047ad676cc880a7d8
SIWON <siwongim033@stu.jejunu.ac.kr>
2023-05-19 03:43:52 +0900

    5월 19일 3시

--- 교보 수정 완.py
+++ 교보 수정 완.py
@@ -1,20 +1,22 @@
-from selenium import webdriver
+#필요한 라이브러리 호출
+from selenium import webdriver #셀레니움:동적 사이트 크롤링 도구
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-import matplotlib.pyplot as plt
-from collections import Counter
-import csv
-import time
+import matplotlib.pyplot as plt #맷플롯립:시각화 도구
+from collections import Counter #카운터: 요소의 개수 세기
+import csv #csv 확장자 입출력
+import time #시간 
 import re
 
-def remove_duplicates(lst):
+def remove_duplicates(lst): #중복 없애기 함수 정의
     return list(set(lst))
 
-book_info_list = []
-name = []
-tags = []
-rt = []
-years = []
+#리스트 정의
+book_info_list = [] #책 정보 저장
+name = [] #책 제목 저장
+tags = [] #책 태그 저장
+rt = [] #가공한 태그 저장
+years = [] #책 발행년도 저장
 query_txt = "디지털" #input("검색 단어 입력 : ") # 검색창에 검색할 단어 입력
 
 path = r"C:\Users\siwon\Desktop\크롤링\chromedriver.exe"
@@ -26,61 +28,63 @@ time.sleep(3) #3초 대기
 links = [] #순회할 리스트
 
 while len(book_info_list) < 100:  # 100개의 정보를 수집할 때까지 반복
-    link_list = driver.find_elements_by_class_name("prod_info")
+    link_list = driver.find_elements_by_class_name("prod_info") 
+    #클래스 이름이 prod_info인 요소를 찾아 변수에 저장
     for link_element in link_list:
-        href = link_element.get_attribute('href')
-        if 'S000' in href:  # 링크에 'S000'이 포함되어 있는지 확인
+        href = link_element.get_attribute('href')#링크를 변수에 저장
+        if 'S000' in href:  # 링크에 'S000'이 포함되어 있으면 저장 / S0000가 들어가야 도서
             links.append(href)
 
-    linkss = remove_duplicates(links)
-    n = len(linkss)
+    linkss = remove_duplicates(links) #반복되는 링크 저장
+    n = len(linkss) #저장된 링크 수
     print("링크 수집 완료")
 
-    for link in linkss:
-        tags = []
-        year = []
-        time.sleep(2)
-        driver.get(link)
+    for link in linkss: #저장한 링크 순회
+        tags = [] #태그 저장 리스트 정의
+        year = [] #발행년도 저장 리스트 정의 
+        driver.get(link) #드라이버가 제어할 사이트 불러오기
+        time.sleep(2) #2초 기다리기 / 로딩이 다 될때까지
 
-        div_element = driver.find_element_by_css_selector(".prod_info_text.publish_date")
-        text = div_element.text
-        pattern = r"(\d+년)"
-        match = re.search(pattern, text)
+        div_element = driver.find_element_by_css_selector(".prod_info_text.publish_date") #발행년도 요소 추출
+        text = div_element.text   #텍스트로 저장
+        pattern = r"(\d+년)" #정규식
+        match = re.search(pattern, text) #이 패턴을 찾아서 변수에 저장
         if match:
-            year = match.group(1)
+            year = match.group(1) #이 패턴이 맞으면 저장
         else:
-            year = "텍스트를 찾을 수 없음"
+            year = "텍스트를 찾을 수 없음" #이 패턴이 아니면 텍스트 출력
             
-        if year not in ['2017년', '2018년', '2019년', '2020년', '2021년', '2022년']:
+        if year not in ['2017년', '2018년', '2019년', '2020년', '2021년', '2022년']: #이 년도의 책이 아니면 for문의 처음으로 돌아가기
             continue
 
-        book_name = driver.find_elements_by_class_name('prod_title')
-        name = book_name[0].text if len(book_name) > 0 else "책 제목 없음"
+        book_name = driver.find_elements_by_class_name('prod_title') #책 제목 요소 추출
+        name = book_name[0].text if len(book_name) > 0 else "책 제목 없음" #값이 있으면 텍스트로 저장, 없으면 텍스트 출력
 
-        book_cate = driver.find_elements_by_css_selector('.intro_book ul li a:nth-child(3)')
-        categories = [elem.text for elem in book_cate]
+        book_cate = driver.find_elements_by_css_selector('.intro_book ul li a:nth-child(3)') #책의 카테고리 요소 추출
+        categories = [elem.text for elem in book_cate]  #저장
 
-        book_tag = driver.find_elements_by_class_name('tab_text')
-        for element in book_tag:
-            tags.append(element.text)
-        rt = tags[13:-8]
+        book_tag = driver.find_elements_by_class_name('tab_text') #책의 태그 요소 추출
+        for element in book_tag: 
+            tags.append(element.text) #추출한 요소 추출
+        rt = tags[13:-8] #요소 데이터 전처리
         rt = [elem for elem in rt if elem != "교환/반품/품절"]
 
-        book_info = [name, year, categories, rt]
+        book_info = [name, year, categories, rt] 
+        #책 정보(책 제목, 발행년도, 카페고리, 태그) 저장
 
-        book_info_list.append(book_info)
-        print(len(book_info_list))
+        book_info_list.append(book_info) #책 정보를 리스트에 저장
+        print(len(book_info_list)) #리스트의 요소 개수 출력
 
-        if len(book_info_list) >= 100:
+        if len(book_info_list) >= 100: #개수가 100개 되면 for문을 나가기
             break
 
-    else : 
+    else : #100개가 안되면 다음 페이지로
         driver.get(r"https://search.kyobobook.co.kr/search?keyword=%EB%94%94%EC%A7%80%ED%84%B8&target=total&gbCode=TOT&page=2&ra=qntt&len=100")
 
-print(book_info_list)
+print(book_info_list) #100개의 책 정보 리스트 출력
 
-driver.quit()
+driver.quit() #드라이버 종료
 
-with open('output.csv', 'w', newline='') as csvfile:
+with open('output.csv', 'w', newline='') as csvfile: #100개의 책 정보를 csv파일로 저장
     writer = csv.writer(csvfile)
     writer.writerows(book_info_list)
--- 교보 수정 완2.py
+++ 교보 수정 완2.py
@@ -1,4 +1,5 @@
-import csv
+#필요한 라이브러리 호출
+import csv 
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 from collections import Counter
@@ -8,30 +9,31 @@ yyyy= []
 font_path = 'C:\Windows\Fonts\gulim.ttc'
 fontprop = fm.FontProperties(fname=font_path, size=12)
 
-with open('output.csv', 'r', newline='') as csvfile:
+with open('output.csv', 'r', newline='') as csvfile: #앞에서 저장한 csv파일 가져오기
     reader = csv.reader(csvfile)
     for row in reader:
         book_info_list.append(row)
-
+#csv파일에서 발행년도 데이터 가져오기        
 for i in range(100):
     yyyy.append(book_info_list[i][1])
 
-yyyy = tuple(yyyy)
-counts = Counter(yyyy)
+yyyy = tuple(yyyy) #발행년도 데이터를 튜플로 저장
+counts = Counter(yyyy) #발행년도 개수 세기
 
-x_list = list(counts.keys())
-X = [item.strip('년') for item in x_list]
-y = list(counts.values())
+x_list = list(counts.keys()) #튜플의 키값을 리스트로 저장
+X = [item.strip('년') for item in x_list] #년 이라는 문자열 삭제
+y = list(counts.values()) #튜플의 밸류값을 리스트로 저장
 
-sorted_data = sorted(zip(X, y))
-X_sorted, y_sorted = zip(*sorted_data)
+sorted_data = sorted(zip(X, y)) 
+#두 리스트를 연결하고 정렬 / 두 리스트를 연결하지 않고 정렬하면 각 값이 달라짐
+X_sorted, y_sorted = zip(*sorted_data)#연결해 정렬한 데이터를 각각 새로운 리스트로 저장
 
-plt.plot(X_sorted, y_sorted,'b-', marker='o')
+plt.plot(X_sorted, y_sorted,'b-', marker='o') #선그래프 그리기
 for i, j in zip(X_sorted, y_sorted):
     plt.text(i, j, str(j), ha='center', va='bottom')
 
-plt.xlabel('년도',fontproperties=fontprop)
-plt.ylabel('권 수',fontproperties=fontprop)
+plt.xlabel('년도',fontproperties=fontprop) #x축에 년도 리스트 지정
+plt.ylabel('권 수',fontproperties=fontprop) #y축에 권수 리스트 지정
 plt.title('교보문고',fontproperties=fontprop)
 plt.grid(True)
 plt.show()
