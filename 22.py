5d6ff400529bf7daa83fac3a9e29bf52ed0d10b7
SIWON <siwongim033@stu.jejunu.ac.kr>
2023-05-19 02:58:55 +0900

    "그래프 출력"

--- 22.py
+++ 22.py
@@ -1,9 +1,12 @@
 import csv
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
 from collections import Counter
 
 book_info_list = []
 yyyy= []
+font_path = 'C:\Windows\Fonts\gulim.ttc'
+fontprop = fm.FontProperties(fname=font_path, size=12)
 
 with open('output.csv', 'r', newline='') as csvfile:
     reader = csv.reader(csvfile)
@@ -15,13 +18,20 @@ for i in range(100):
 
 yyyy = tuple(yyyy)
 counts = Counter(yyyy)
-print(counts)
 
 x_list = list(counts.keys())
-y_list = list(counts.values())
+X = [item.strip('년') for item in x_list]
+y = list(counts.values())
 
-plt.bar(x_list, y_list)
-plt.xlabel('Year')
-plt.ylabel('Count')
-plt.title('Count of Year')
+sorted_data = sorted(zip(X, y))
+X_sorted, y_sorted = zip(*sorted_data)
+
+plt.plot(X_sorted, y_sorted,'b-', marker='o')
+for i, j in zip(X_sorted, y_sorted):
+    plt.text(i, j, str(j), ha='center', va='bottom')
+
+plt.xlabel('년도',fontproperties=fontprop)
+plt.ylabel('권 수',fontproperties=fontprop)
+plt.title('교보문고',fontproperties=fontprop)
+plt.grid(True)
 plt.show()
